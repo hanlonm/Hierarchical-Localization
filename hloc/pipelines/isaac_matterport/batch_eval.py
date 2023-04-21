815ec68df98f700690adceec366954f3e54bfc4b
Matthew Hanlon <hanlonm@ethz.ch>
2023-04-21 12:18:01 +0200

    continued work on eval

--- hloc/pipelines/isaac_matterport/batch_eval.py
+++ hloc/pipelines/isaac_matterport/batch_eval.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from pytransform3d import transformations as pt
 from pytransform3d import rotations as pr
 import numpy as np
+import os
 
 
 environment = '00195'
@@ -15,6 +16,9 @@ outputs = Path('/local/home/hanlonm/Hierarchical-Localization/outputs/'+environm
 loc_pairs = outputs / 'pairs-query-netvlad20.txt'  # top 20 retrieved by NetVLAD
 results = outputs / (environment + '_hloc_superpoint+superglue_netvlad20.txt')  # the result file
 local_features = outputs / 'feats-superpoint-n4096-rmax1600.h5'
+
+# Evaluation Paths
+run_path = Path('/local/home/hanlonm/mt-matthew/eval_results/run_1')
 
 feature_conf = extract_features.confs['superpoint_max']
 matcher_conf = match_features.confs['superglue']
@@ -51,8 +55,44 @@ T_cam_base = pt.transform_from(
             np.array([[0.0, -1.0, 0.0], [0.0, 0.0, -1.0], [1.0, 0.0, 0.0]]),
             np.array([0, 0.0, 0.0]))
 
-localization_results: dict = logs['loc']
+T_world_map = pt.transform_from(
+            np.array([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]]),
+            np.array([0, 0.0, 1.8]))
 
+localization_results: dict = logs['loc']
+sorted_result_keys = list(localization_results.keys())
+sorted_result_keys.sort()
+
+trajectory_dirs = os.listdir(run_path)
+
+for trajectory_dir in trajectory_dirs:
+    trajectory_results = [key for key in sorted_result_keys if trajectory_dir in key]
+    trajectory_variations = os.listdir( run_path / trajectory_dir)
+
+    for variation in trajectory_variations:
+        variation_results = [key for key in trajectory_results if variation[:-4] in key]
+        pose_estimates = np.zeros((len(variation_results), 7))
+        pose_file = open(run_path/ trajectory_dir / (variation[:-4] + '_loc_est.txt'), "w")
+        pose_file.write("# tx ty tz qw qx qy qz points_detected num_PnP_inliers")
+        for image_result_key in variation_results:
+            result = localization_results[image_result_key]
+            tvec = result['PnP_ret']['tvec']
+            qvec = result['PnP_ret']['qvec']
+            rot = pr.matrix_from_quaternion(qvec)
+            T_cam_world = pt.transform_from(rot, tvec)
+
+            T_map_base = np.linalg.inv(T_cam_world) @ T_cam_base
+            T_world_base = T_world_map @ T_map_base
+
+            pq = pt.pq_from_transform(T_world_base)
+            loc_stats = np.append(pq, [len(result['points3D_ids']), result['PnP_ret']['num_inliers']])
+
+            pose_file.write("\n" + " ".join(map(str, loc_stats)))
+        pose_file.close()    
+            
+
+
+    
 
 
 
