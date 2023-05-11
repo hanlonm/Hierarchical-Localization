b8a5f68fce49aa126916cf7bf23d6426c5b97fcf
Matthew Hanlon <hanlonm@ethz.ch>
2023-05-11 16:18:30 +0200

    adding command line arguments for cluster

--- hloc/pipelines/isaac_matterport/build_aligned_map_HL.py
+++ hloc/pipelines/isaac_matterport/build_aligned_map_HL.py
@@ -1,48 +1,59 @@
 import os
+import argparse
 from pathlib import Path
 
 from hloc import extract_features, match_features, reconstruction, visualization, pairs_from_exhaustive, pairs_from_retrieval
 from hloc.visualization import plot_images, read_image
 from hloc.utils import viz_3d
 
-
-home_dir = os.environ.get("CLUSTER_HOME", "/local/home/hanlonm")
-environment = "00195_HL_SPA_NN"
-images = Path(home_dir + '/Hierarchical-Localization/datasets/'+environment)
-outputs = Path(home_dir+ '/Hierarchical-Localization/outputs/'+environment+'/')
-sfm_pairs = outputs / 'pairs-netvlad.txt'
-sfm_dir = outputs / 'reconstruction'
-
-
-retrieval_conf = extract_features.confs['netvlad']
-feature_conf = extract_features.confs['superpoint_aachen']
-matcher_conf = match_features.confs['NN-superpoint']
-
-retrieval_path = extract_features.main(retrieval_conf, images, outputs)
-pairs_from_retrieval.main(retrieval_path, sfm_pairs, num_matched=20)
+def build_map(environment):
+    home_dir = os.environ.get("CLUSTER_HOME", "/local/home/hanlonm")
+    images = Path(home_dir + '/Hierarchical-Localization/datasets/'+environment)
+    outputs = Path(home_dir+ '/Hierarchical-Localization/outputs/'+environment+'/')
+    sfm_pairs = outputs / 'pairs-netvlad.txt'
+    sfm_dir = outputs / 'reconstruction'
 
 
-feature_path = extract_features.main(feature_conf, images, outputs)
-matches = match_features.main(matcher_conf, sfm_pairs, feature_conf['output'], outputs)
+    retrieval_conf = extract_features.confs['netvlad']
+    feature_conf = extract_features.confs['superpoint_aachen']
+    matcher_conf = match_features.confs['NN-superpoint']
 
-model = reconstruction.main(sfm_dir, images, sfm_pairs, feature_path, matches, verbose=True)
+    retrieval_path = extract_features.main(retrieval_conf, images, outputs)
+    pairs_from_retrieval.main(retrieval_path, sfm_pairs, num_matched=20)
 
-image_names = []
-locations = []
 
-pose_file = images / "image_poses.txt"
-with open(pose_file) as f:
-        for line in f.readlines():
-            line = line.rstrip('\n')
-            if line[0] == '#' or line == '':
-                continue
-            data = line.replace(',', ' ').split()
-            image_names.append(data[0])
-            locations.append([float(data[1]),float(data[2]),float(data[3])])
+    feature_path = extract_features.main(feature_conf, images, outputs)
+    matches = match_features.main(matcher_conf, sfm_pairs, feature_conf['output'], outputs)
 
-similarity_transform = model.align_robust(image_names, locations, 3, max_error=12.0, min_inlier_ratio=0.1)
-model.write(sfm_dir)
+    model = reconstruction.main(sfm_dir, images, sfm_pairs, feature_path, matches, verbose=True)
 
-fig = viz_3d.init_figure()
-viz_3d.plot_reconstruction(fig, model, color='rgba(255,0,0,0.5)', name="mapping")
-fig.show()
+    image_names = []
+    locations = []
+
+    pose_file = images / "image_poses.txt"
+    with open(pose_file) as f:
+            for line in f.readlines():
+                line = line.rstrip('\n')
+                if line[0] == '#' or line == '':
+                    continue
+                data = line.replace(',', ' ').split()
+                image_names.append(data[0])
+                locations.append([float(data[1]),float(data[2]),float(data[3])])
+
+    similarity_transform = model.align_robust(image_names, locations, 3, max_error=12.0, min_inlier_ratio=0.1)
+    model.write(sfm_dir)
+
+    fig = viz_3d.init_figure()
+    viz_3d.plot_reconstruction(fig, model, color='rgba(255,0,0,0.5)', name="mapping")
+    fig.show()
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--environment", type=str, default="00700")
+    
+    args = parser.parse_args()
+    build_map(args.environment)
+
+
+if __name__ == "__main__":
+    main()
